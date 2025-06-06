e743c8adda53ba7f728161cf63287497992ac259
proGamalSherif <jemyxsherifx@gmail.com>
2025-06-06 04:15:59 +0300

    Update Online Users

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    apiUrl:`https://localhost:7280/api`,
-    hubUrl:`https://localhost:7280`,
-    // hubUrl:'https://voicechat-backend.runasp.net',
-    // apiUrl: 'https://voicechat-backend.runasp.net/api'
+    // apiUrl:`https://localhost:7280/api`,
+    // hubUrl:`https://localhost:7280`,
+    hubUrl:'https://voicechat-backend.runasp.net',
+    apiUrl: 'https://voicechat-backend.runasp.net/api'
 };
